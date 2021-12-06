4cca7021716e256cf730b588c3a93339c12440fd
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-06 15:18:47 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ $config['no_cache'] = false;
 $config['ui'] = true; //if set to true, module will be visible in the toolbar
 $config['ui_admin'] = false; //if set to true, module will be visible in the admin panel
 $config['position'] = 301;
-$config['version'] = 0.2;
+$config['version'] = 1.2;
 $config['categories'] = "other";
