5363b559e5c3bd4fd65da8b66087f0bd16606e50
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-06 15:14:38 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,14 +4,14 @@
     <div class="mw-ui-field-holder">
         <label class="mw-ui-label"><?php _e("RSS url"); ?></label>
         <div class="range-slider">
-            <input name="rss_url"  class="mw-ui-field-range mw_option_field mw-full-width" type="text"  value="<?php print get_option('rss_url', $params['id']) ?>">
+            <input name="rss_url"  class="mw-ui-field-range mw_option_field mw-full-width form-control" type="text"  value="<?php print get_option('rss_url', $params['id']) ?>">
         </div>
     </div>
 
     <div class="mw-ui-field-holder">
         <label class="mw-ui-label"><?php _e("Number of posts"); ?></label>
         <div class="range-slider">
-            <input name="rss_post_limit"  class="mw-ui-field-range mw_option_field mw-full-width" type="text"  value="<?php print get_option('rss_post_limit', $params['id']) ?>">
+            <input name="rss_post_limit"  class="mw-ui-field-range mw_option_field mw-full-width form-control" type="text"  value="<?php print get_option('rss_post_limit', $params['id']) ?>">
         </div>
     </div>
 </div>
